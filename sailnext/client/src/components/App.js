b9628566bbfe718a2b3cd8a5b0a5d3e3f408e6e5
matteos02 <105994532+matteos02@users.noreply.github.com>
2023-02-03 07:52:36 -0500

    I'm tired of this shit

--- sailnext/client/src/components/App.js
+++ sailnext/client/src/components/App.js
@@ -4,6 +4,7 @@ import jwt_decode from "jwt-decode";
 
 import NotFound from "./pages/NotFound.js";
 import Skeleton from "./pages/Skeleton.js";
+import NavBar from "./parts/NavBar.js";
 
 import "../utilities.css";
 
@@ -43,6 +44,7 @@ const App = () => {
 
   return (
     <>
+      <NavBar />
       <Router>
         <Skeleton path="/" handleLogin={handleLogin} handleLogout={handleLogout} userId={userId} />
         <NotFound default />
---
+++ sailnext/client/src/components/parts/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../utilities.css";
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav>
+        <div className="navContainer">
+          <h1 className="navTitle">S N</h1>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavBar;
